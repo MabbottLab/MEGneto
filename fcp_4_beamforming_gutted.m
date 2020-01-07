114718976abe43bbbc1dc0b26a54acd4edf43581
Julie Tseng <julie.tseng@sickkids.ca>
2020-01-07 10:04:21 -0500

    Removed the dummy fcp_4/5 files; Moved working fcp_4/5 files to top-level folder from dev_functions.

--- fcp_4_beamforming_gutted.m
+++
@@ -1,8 +0,0 @@
-function [source, source_proj] = fcp_4_beamforming_gutted(paths)
-
-% Gutted for pipeline workflow testing
-disp('Just the illusion of beamforming going on here.')
-source = randi(100,100,100);
-source_proj = reshape(source(randperm(100.^2)),[100 100]);
-
-end
--- fcp_5_connectivity_gutted.m
+++
@@ -1,9 +0,0 @@
-function [stat] = fcp_5_connectivity_gutted(paths, source, source_proj)
-
-% Gutted for pipeline workflow testing
-disp('Just the illusion of connectivity analysis here.')
-stat = sum(source.*source_proj);
-pause(2)
-disp('Quasi-done.')
-
-end
