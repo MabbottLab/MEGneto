0de8d3642489cff9e4fdd332e6cdbc4533482aca
[Julie Tseng] <[julie.a.tseng@gmail.com]>
2020-02-25 14:09:25 -0500

    fixing the encoding issue #17 by removing spaces if they exist

--- functions/load_participants.m
+++ functions/load_participants.m
@@ -5,6 +5,7 @@ if nargin < 3
     prefix = 'subj_';
 end
 participant = readtable(paths.([prefix step]), 'ReadVariableNames', false, 'Delimiter', ',');
+participant = rmmissing(cell2table(cellfun(@(x) x(~isspace(x)), table2cell(participant), 'UniformOutput', false)));
 if isempty(participant)
     error(['No participants specified - please add ds files to ' paths.conf_dir '/' prefix step '.csv']);
 end
