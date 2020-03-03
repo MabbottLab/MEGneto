356d00d64732938fbc1535ebb0e4c0ca784d90e5
Julie Tseng <julie.tseng@sickkids.ca>
2020-03-03 14:18:47 -0500

    adding documentation to explain input parameters

--- dev_functions/make_NBS_ready.m
+++ dev_functions/make_NBS_ready.m
@@ -1,9 +1,22 @@
-function make_NBS_ready(paths, group_names, conn)
+function make_NBS_ready(paths, group_names, conn, contrasts)
 
 % To make data matrix and design matrix for NBS exercise 
 % modified from NBS_sam by Sonya - May 9, 2018
 % modified by Liz - Sept 2018, Jan 2020
 % function-ified by Julie - March 2020
+
+% INPUT--------------------------------------------------------------------
+% paths:        the paths struct, as we always input
+% group_names:  all group names as an array of strings, e.g., ["surg",
+%               "rad", "control"], exactly as they appear in folder names
+% conn:         name of connectivity metric as a character array, e.g.,
+%               'wpli_deb'
+% contrasts:    value that design matrix should take on for each group,
+%               thus this should be an array with length (num_groups)
+
+% OUTPUT-------------------------------------------------------------------
+% design_matrix.mat:        design matrix for NBS analysis
+% NBS[freq]_datamatrix.mat: data matrices organized by frequency band
 
 % IMPORTANT: this version assumes that the participant adjmats only have
 % the first entry of the 3rd dimension (the trials dimension) populated.
@@ -13,10 +26,10 @@ function make_NBS_ready(paths, group_names, conn)
 %% SETUP
 
 spath = paths.analyses; % path to analysis folder
-numGroups = length(group_names);
+numGroups = length(group_names); % how many groups?
 
-groupPLI = cell(1,numGroups);
-for gg = 1:numGroups
+groupPLI = cell(1,numGroups); % initialize
+for gg = 1:numGroups % for each group, grab the participants list and save in cell array
     [LIST, ISDIR] = glob(sprintf('%s/%s/ST*/fcp_5_adjmat_%s.mat', spath, group_names(gg), conn));
     groupPLI{gg} = LIST(~ISDIR);
 end
@@ -27,8 +40,8 @@ nbs_datamat = [];
 for gg = 1:numGroups
     fprintf(['\t---- Group ',num2str(gg),' ----\n']);
         for ss = 1:length(groupPLI{gg})
-            this_adjmat = load(groupPLI{gg}{ss});
-            nbs_datamat = cat(3, nbs_datamat, this_adjmat.adjmat(:,:,1,:));
+            this_adjmat = load(groupPLI{gg}{ss}); % load the struct
+            nbs_datamat = cat(3, nbs_datamat, this_adjmat.adjmat(:,:,1,:)); % throw in the participant's conn
         end
 end
 
@@ -50,7 +63,7 @@ end
 design_matrix = [];
 for gg = 1:numGroups
     this_gp = zeros(1,numGroups);
-    this_gp(gg) = 1;
+    this_gp(gg) = contrasts(gg);
     design_matrix = vertcat(design_matrix, repmat(this_gp, length(groupPLI{gg}), 1));
 end
 
