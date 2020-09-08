10e65351e6f1281de5068144c05c8fe6dab993c1
Julie Tseng <julie.a.tseng@gmail.com>
2020-09-08 16:29:55 -0400

    clarifying each step and inputs

--- mainSurf_rest.m
+++ mainSurf_rest.m
@@ -36,29 +36,25 @@ rawdata_path        = '/home/megneto/data/sbells/meg_freesurfer/MEG';
 mri_path            = '/home/megneto/data/sbells/meg_freesurfer/MRIs';
 paths               = megne2setup(project_path, analysis_name, rawdata_path, mri_path, false);
 
-%% fcp_1: task epoching, jump/muscle artifact detection, bad channel detection
+%% fcp_1: task epoching, jump/muscle artifact detection, bad channel det
 
 % write participant IDs to the appropriate *.csv, helper lines
 MEG_ds = struct2table(dir(paths.rawdata)); % get all possible IDs
 writecell(MEG_ds.name(3:(height(MEG_ds))), paths.('subj_fcp1')); % write them
 
-% run analysissurf_path = '/home/megneto/data/sbells/meg_freesurfer/processed_freesurfer';
-
+% run analysis
 fcp_1_RestingStateEpoching(paths)
 
-%% fcp_2: ICAsurf_path = '/home/megneto/data/sbells/meg_freesurfer/processed_freesurfer';
-
+%% fcp_2: ICA
 
 % write PIDs to appropriate *.csv, ASSUMING no change in participants to be
-% analyzedsurf_path = '/home/megneto/data/sbells/meg_freesurfer/processed_freesurfer';
-
+% analyzed
 writecell(MEG_ds.name(3:(height(MEG_ds))), paths.('subj_fcp2'));
 
-% run analysissurf_path = '/home/megneto/data/sbells/meg_freesurfer/processed_freesurfer';
+% run analysis
+fcp_2_RestPreprocessingICA(paths)
 
-fcp_2_PreprocessingICA(paths)
-
-%% fcp_2_5: INTERACTIVE - identifies bad ICA components, reject those components
+%% fcp_2_5: INTERACTIVE - identify bad ICA components (human)
 
 % write PIDs then modify according to who you want to remove
 writecell(MEG_ds.name(3:(height(MEG_ds))), paths.('subj_fcp2_5'));
@@ -74,26 +70,24 @@ writecell(MEG_ds.name(3:(height(MEG_ds))), paths.('subj_fcp3'));
 % run channel repair
 fcp_3_ChannelRepair(paths)
 
-%% fcp_4a: register freesurfer surfaces to ctf mri
-% user interaction needed for ac-pc labeling and freesurfer determine
-% coordinate (RAS)
+%% fcp_4a: INTERACTIVE - register freesurfer surfaces to ctf mri
+%  user interaction needed for ac-pc labeling and freesurfer determine
+%  coordinate system (RAS)
 
 % write PIDs for analysis
 writecell(MEG_ds.name(3:(height(MEG_ds))), paths.('subj_fcp4'));
 
-% specify path to freesurfer surfaces
-surf_path = '/home/megneto/data/sbells/meg_freesurfer/processed_freesurfer';
-
 % run registration
 fcp_4a_register_CTF_Surf(paths,surf_path)
 
+%%% SONYA'S NOTES ON OIRM PARTICIPANTS-------------------------------------
 % no acpc:
-% good : 02,03,07,24,26
-% bad  : 06,11,15,16,17,27,28,
+%   good : 02,03,07,24,26
+%   bad  : 06,11,15,16,17,27,28,
 
 % w acpc (spm12)
-% good : 02,03,06,07,11,15,16,17,24,26,27,28,29,30,31,39,41,43,44,47
-% 16??17??
+%   good : 02,03,06,07,11,15,16,17,24,26,27,28,29,30,31,39,41,43,44,47
+%   16??17??
 
 %% fcp_4: beamforming surfaces
 
@@ -102,10 +96,14 @@ writecell(MEG_ds.name(3:4), paths.('subj_fcp4'));
 
 % run beamforming
 fcp_4_Beamforming_Freq_Surf(paths)
-%alpha : (8-13) 10 : 2 
-%theta : (4-8) 5 : 2
-%beta : 13-30 ; 20 : 7
-%gamma (30-48); 39 : 9
+
+% NOTE TO USERS: 
+% freq_band : [min, max], center, +/- value
+% -------------------------------------------
+% theta     : [ 4,  8],   5,  2
+% alpha     : [ 8, 13],  10,  2 
+% beta      : [13, 30],  20,  7
+% gamma     : [30, 48],  39,  9
 
 %% fcp_5: connectivity
 
