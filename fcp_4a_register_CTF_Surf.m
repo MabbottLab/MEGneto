b3fa59cc3425eff5a377c496ae4849453817bf34
Julie Tseng <julie.a.tseng@gmail.com>
2020-09-08 14:53:19 -0400

    final documentation clean up, removing test lines that are outdated / unused, etc.

--- fcp_4a_register_CTF_Surf.m
+++ fcp_4a_register_CTF_Surf.m
@@ -1,11 +1,7 @@
 function fcp_4a_register_CTF_Surf(paths, surf_path)
 
-% fcp_4_Beamforming_Freq_Surf does the following:
-% 1) registers CTF mri files with already processed
-% freesurfer surf structures and hcp-workbench processing
-% 2) frequency analysis on cleaned data
-% 3) then carries out beamforming and source projection 
-%
+% FCP_4A_REGISTER_CTF_SURF involves interactive steps to align MEG and MRI 
+% scans to facilitate a successful coregistration.
 %
 % NOTES:
 %   - Ensure that subj_fcp4.csv is populated with the subject IDs of
@@ -59,8 +55,8 @@ end
 
 rangeOFsubj = 1:length(subj_match.ds);
 
+%%% PREPARE FOR COREGISTRATION --------------------------------------------
 for ss = rangeOFsubj
-    %%% PREPARE FOR COREGISTRATION-----------------------------------------
     fprintf('\nWorking on subject %s! \n', subj_match.pid{ss});
 
     % set up paths to MRI, freesurfer surface, data locations
@@ -68,7 +64,7 @@ for ss = rangeOFsubj
     datapath        = fullfile(mripath,'freesurfer/workbench');
     freesurferpath  = fullfile(mripath,'freesurfer/mri');
 
-    % load ctf image (mm units)
+    % load CTF MRI (mm units)
     mri             = ft_read_mri([paths.rawmri '/' subj_match.pid{ss} '_T1_V2.mri']);
 
     % check for fiducials
@@ -76,7 +72,8 @@ for ss = rangeOFsubj
         error('No fiducials found for subject %s!', subj_match.pid{ss});
     end
 
-    % mark AC space for better co-registration compatibility
+    % INTERACTIVE: FIND LANDMARKS ON CTF MRI --------------------------------
+    % mark AC, PC, right side, superior for better co-registration compatibility
         % step 1: use three images to find tip of anterior commissure
         % step 2: press 'a' on keyboard to mark ac 
         %         press 'p' on keyboard to mark pc 
@@ -95,21 +92,13 @@ for ss = rangeOFsubj
     % need to indicate ras coordinate system (right, ant, superior)
     t1              = ft_read_mri([freesurferpath, '/norm.mgz']);
     t1              = ft_determine_coordsys(t1, 'interactive', 'yes');
- 
-%     % skull-strip  the anatomical CTF MRI
-%     cfg               = [];
-%     cfg.output        = 'skullstrip';
-%     cfg.viewresult    = 'yes';
-%     mri_acpc_brain    = ft_volumesegment(cfg, mri_acpc);
-   
-    %%% REGISTER FreeSurfer T1 -> CTR MRI ---------------------------------
+    
+%%% REGISTER FreeSurfer T1 -> CTF MRI -------------------------------------
     cfg                 = [];
     cfg.method          = 'spm';
-    % cfg.spmversion = 'spm8';
     cfg.spmversion      = 'spm12';
     cfg.viewresult      = 'yes';
     mri_t12acpc         = ft_volumerealign(cfg, t1, mri_acpc);
-    % mri_t12acpc       = ft_volumerealign(cfg, t1, mri_acpc_brain);
     
     % save transform
     transform_t12ctf    = mri_t12acpc.transform; 
@@ -124,20 +113,20 @@ for ss = rangeOFsubj
     cfg.parameter   = 'anatomy';
     ft_volumewrite(cfg, mri_t12acpc);
     
-    % freesurfer -> acpc -> ctf
+    % get transformation matrix for: freesurfer -> acpc -> ctf
     transform_surf2mri = mri.transform/mri_acpc.transform * ...
                          mri_t12acpc.transform/mri_t12acpc.transformorig;
    
-    %%% LOAD FREESURFER SURFACE--------------------------------------------
+%%% LOAD FREESURFER SURFACE--------------------------------------------
     filename            = fullfile(datapath,[subj_match.pid{ss},'.L.midthickness.4k_fs_LR.surf.gii']);
     sourcemodel_4korig  = ft_read_headshape({filename, strrep(filename, '.L.', '.R.')});
 
-    % transform to ctf space
+    % transform to CTF space
     sourcemodel_4k      = ft_transform_geometry(transform_surf2mri, sourcemodel_4korig);
-    sourcemodel_4k.inside = sourcemodel_4k.atlasroi>0;
+    sourcemodel_4k.inside = sourcemodel_4k.atlasroi > 0;
     sourcemodel_4k      = rmfield(sourcemodel_4k, 'atlasroi');
     
-    %%% CREATE HEAD MODEL---------------------------------------------------
+%%% CREATE HEAD MODEL---------------------------------------------------
 
     % load template_grid (can create your own template grid)
     % NOTE: the path to the template file is user-specific
@@ -166,10 +155,10 @@ for ss = rangeOFsubj
     save(fullfile(ssSubjPath(ss),sprintf('/%s_sourcemodel_4k',subj_match.pid{ss})), 'sourcemodel_4k');
     save(fullfile(ssSubjPath(ss),sprintf('/%s_hdm',subj_match.pid{ss})), 'headmodel');
 
-     %%% LOAD MEG DATA ---------------------------------------------------------
+%%% LOAD MEG DATA ---------------------------------------------------------
     load([ssSubjPath(ss) '/ft_meg_fullyProcessed.mat'],'-mat','data');
 
-    %% VISUALIZATION: Headmodel and Surfaces --------------------------------
+%% VISUALIZATION: Headmodel and Surfaces --------------------------------
     close all;
     
     figure;
