a4c0eac84df27e156f9f98068b374bbf1dd960bb
dunjamatic <matic.dunja@gmail.com>
2021-04-16 14:20:53 -0400

    uncommented diary

--- fcp_5_freqanalysis.m
+++ fcp_5_freqanalysis.m
@@ -30,7 +30,7 @@ function fcp_5_freqanalysis(paths)
 
 right_now = clock;
 log_filename = [paths.conf_dir '/log_' sprintf('%02.f%02.f%02.f', right_now(1:3))];
-%diary(log_filename)
+diary(log_filename)
 
 fprintf('\n\n%02.f:%02.f:%02.f       Now running **%s**.\n', ...
     right_now(4:6), mfilename)
