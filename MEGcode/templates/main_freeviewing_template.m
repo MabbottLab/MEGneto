684c03b64a65ecab784b0cc92ae5c8b7f7475b5a
dunjamatic <matic.dunja@gmail.com>
2021-08-20 13:42:45 -0400

    editing the main template

--- MEGcode/templates/main_freeviewing_template.m
+++ MEGcode/templates/main_freeviewing_template.m
@@ -96,6 +96,10 @@ fclose(fid); % close the file
 % detect and reject trials with excessive head motion and muscle/jump
 % artifacts, and detect and record (not reject) bad channels.
 
+% Note that this step uses freeviewingTaskTrialFunc where the output of the
+% clip times extraction step (done prior to using this freeviewing MEGneto
+% pipeline), is loaded to facilitate trial epoching.
+
 freeviewing_fcp_1_TaskEpoching(paths) % run first step of the MEG pipeline
 
 % after fcp_1, check output for: not enough trials, excessive head motion, 
@@ -146,6 +150,10 @@ freeviewing_fcp_3_ChannelRepair(paths)
 % subj_fcp4.csv and remove any participants you don't wish to include for 
 % fcp4 and subsequent steps.
 
+% Note that this step saves two parts of beamforming data for each run of
+% participant data. This is done to facilitate beamforming at a finer grid
+% resolution. 
+
 % Uncomment the following to remove NAN channels for Run 1 data of
 % participant P02. Previously this channel was found to be channel 182, but
 % the user may change this channel number if they find otherwise.
@@ -158,6 +166,16 @@ freeviewing_fcp_3_ChannelRepair(paths)
 
 freeviewing_fcp_4_beamforming(paths)
 
+%% fcp_4_5: reinserting trials
+% This step stacks the output of the beamforming step for each participant 
+% (i.e., it combines all beamforming from both runs of a participant's MEG 
+% data) and inserts blank columns for trials that were rejected in the task 
+% epoching step. 
+
+% This step is not meant to be run as a function. Instead, the user should
+% open the script and run each section of code as specified at the top of
+% that file.
+
 %% fcp_5: frequency analysis 
 % This step performs frequency analysis to generate power spectrum data
 % which can be used to test hypotheses based on spectral power.
@@ -167,172 +185,24 @@ freeviewing_fcp_4_beamforming(paths)
 % subj_fcp5.csv and remove any participants you don't wish to include for 
 % fcp5 and subsequent steps.
 
+% Note that this step loads in a feature vector that discriminates
+% categories of visual content. The user must specify which category
+% should be analyzed, as only one category is run at a time. For instance,
+% if the user wishes to analyse the faces category first, they must specify
+% that in the freeviewing_fcp_5_freqanalysis function and alter the name of
+% the saved file, and then run the function. Then, they must change the 
+% category to scenes, change the output name to reflect the scenes
+% category, and re-run the function. If the user wishes to analyse all
+% categories at once (i.e., not separate it out into groups), the user must
+% specify that as well. All of this is specified in lines 135-137.
+
 freeviewing_fcp_5_freqanalysis(paths);
 
-%% The functions below are un-used in the freeviewing data analysis.
-%% fcp_5: connectivity
-% This step estimates functional connectivity (neural synchrony between
-% regions) using a specified connectivity metric from the JSON config file.
+%% fcp_5_5: statistical analysis
+% This step performs statistical analysis and creates visual 
+% representations of results for each aim and hypothesis of the 
+% OIRM Faces v Scenes Summer 2021 research project. 
 
-fcp_5_taskconnectivity(paths);
-
-%% Additional functions
-% The sections below contain additional functions that may be run after
-% the final fcp step of the pipeline. These functions serve as additional
-% analysis/analysis preparation tools.
-
-%% Preparation functions
-% includes: make_NBS_ready, make_BNV_ready
-
-%% make_NBS_ready 
-% This function prepares a design matrix to serve as input to the
-% NBS toolbox. The design matrix columns are the participant groups
-% (e.g. "control", "surgery", etc.) and rows are participants. A "0" or
-% "1" indicates whether the participant belongs to the group/column ("1") 
-% or not ("0"). This function also outputs a list of participant
-% (ordered_ppt_list.txt) in the analysis folder to inform the user of the
-% design matrix's rows (same order as ppt list prestented in the .txt file).
-% IMPORTANT: the by-frequency output files also have re-arranged
-% participants that match the order of ordered_ppt_list.txt.
-
-% Don't forget to include a nickname_ParticipantCategories.xlsx file 
-% (where nickname corresponds to the subAnalysis_name variable below) in 
-% your config folder. An example of this excel sheet with dummy variables
-% is available in the templates folder (note that the column names of this
-% file, in order, represent radiation, sugery, and typical development 
-% controls). 
-% Fill in the variables below which are the inputs to the function.
-
-% Specify function inputs
-group_names = NaN; % array of strings, e.g., ["surg", "rad", "control"], 
-                    % exactly as it appears in the participant categories
-                    % sheet
-conn = NaN; % name of connectivity metric as a character array (must match 
-%            the metric outlined in the file name of the connectivity
-%            matrix .mat file). Can take on values including: 
-%            "plv, "pli", "wpli", "wpli_debiased", "coh"
-
-subAnalysis_name = NaN; % nickname for sub-analysis, e.g. 'TreatVsControl'
-                        % this gets prepended to design_matrix and NBS data
-                        % mat files. This should also match the name of
-                        % the corresponding *_ParticipantCategories.xlsx
-
-make_NBS_ready(paths, group_names, conn, subAnalysis_name)
-%% make_BNV_ready
-% This fuction creates *.node and *.edge files for viewing connectivity 
-% results from PLS or NBS on BrainNet Viewer (BNV). 
-
-% Don't forget to create the 'brainnet' struct containing nine user 
-% specified parameters to pass into this function. 
-
-% Specify function inputs
-brainnet = NaN; % struct created by the user. Refer to documentation in the 
-%                file 'make_BNV_ready.m' for a descrption of the parameters
-%                in 'brainnet'.
-
-make_BNV_ready(paths, brainnet)
-
-%% Statistical analysis functions
-% includes: bootTestDiffSeeds
-
-%% bootTestDiffSeeds
-% This function performs permutation-based significance testing (via 
-% t-test or f-test using the max procedure) to build a null distribution 
-% and control for Type 1 error.
-
-% Note on T-max analysis extra input:
-% If you wish to perform a T-max analsysis, you must include a .xlsx file
-% with two groups that will be compared in the T-max analysis for 
-% differences. You may use the make_NBS function to prepare this file
-% where your input to make_NBS (the ParticipantCategories.xlsx) should have
-% two groups you wish to include (e.g. "control" and "surg") and the
-% participant IDs that fall under each group. 
-
-% Specify function inputs
-bootcfg.seed_regions = [1, 2, 3];      % numeric indices indicating the seed 
-%                                       ROIs (e.g. if the AAL atlas is used, 
-%                                       the default input [1, 2, 3] 
-%                                       corresponds to the following regions 
-%                                       ['left precentral gyrus', 'right 
-%                                       precentral gyrus', 'left superior 
-%                                       frontal gyrus, dorsolateral']. Note 
-%                                       that for AAL atlas there are 90 
-%                                       regions, so indices should take on 
-%                                       values between 1-90). 
-bootcfg.freq_band = 'gamma';          % frequency band of interst 
-%                                       (e.g. 'alpha', 'beta', 'gamma', 
-%                                       'theta')
-bootcfg.num_bootstraps = 1000;        % number of desired bootstrap tests. 
-%                                       Default is 1000.
-bootcfg.thresh = 0.05;                % significance threshold for the 
-%                                       p-value. Default is 0.05, can be 
-%                                       altered to desired threshold 
-%                                       by the user.
-bootcfg.group_names = ["rad", "surg", "tdc"]; % array of strings, e.g., 
-%                                       ["RAD", "SURG", "TDC"], 
-%                                       exactly as they appear in the input
-%                                       for group_names in the make_NBS
-%                                       function.
-bootcfg.nickname = 'nickname';         % subAnalysis name you set in make_NBS_ready
-
-bootTestDiffSeeds(paths, bootcfg)
-
-%% Summary functions
-% includes: inspecting_results, getTrialSummary,
-% getMarkerSummary,reactionTimes, trialSummary
-
-%% inspecting_results
-% This function allows the user to analyze the pipeline results by
-% visualizing the data as specified by the user (in type.viz).
-
-% Don't forget to include a ParticipantCategories.xlsx file in your
-% paths.conf_dir folder and a 'type' struct as input.
-
-% Specify function inputs
-name = NaN; % name of the group output file the user wishes to inspect from 
-%            fcp_5_freqanalysis (e.g. 'fcp_5_powspctrm_blcorrected.mat') or
-%            fcp_5_taskconnectivity 
-%            (e.g. 'fcp_5_allParticipants_conn_mats_wpli_debiased.mat')
-type = NaN; % user-created struct with specifications for inspecting the 
-%            data. Refer to the documentation in 'inspecting_results.m' 
-%            for an example of the parameters included in the 'type'. 
-
-inspecting_results(paths, name, type)
-
-%% getTrialSummary
-% This function summarizes trial information for each participant, such as
-% the number of trials, the number of trials removed due to head motion, 
-% number of trials removed due to noise, etc. 
-
-% Specify function inputs
-num_markers = NaN; % number of events expected 
-                   % (total number of times stimulus is presented)
-thresh = 25;       % percentage indicating what percentage of trials removed 
-                   % is unacceptable. Here, 25 is the lab's convention.
-
-getTrialSummary(paths, num_markers, thresh)
-
-%% getMarkerSummary
-% This function summarizes the markers for each participant and generates 
-% a cluster which describes redundancies in the markers.
-
-getMarkerSummary(paths)
-
-%% reactionTimes
-% This function extracts participant reaction times (time between stimulus 
-% presentation and button press). Currently, this function is used for the 
-% Speed of Thinking TP & EL dataset.
-
-% This function is run by uncommenting certain lines within
-% fcp_1_TaskEpoching.m. These lines include: 96, 97, 98, 102, 141, 142,
-% 237, 238). Please read the reactionTimes.m file in the dev_functions
-% folder for more details and information on where the output is stored. 
-
-%% trialSummary
-% This function generates a summary of the number of correct trials,
-% incorrect trials and total trials per participant. This function must be
-% run after at least fcp_1. Please read the reactionTimes.m file in the 
-% dev_functions folder for more details and information on where the output 
-% is stored. 
-
-trialSummary(paths)
+% This step is meant to be run in sections, not as a function. The user 
+% should open the script and refer to it for detailed instructions on
+% each section of the script.
