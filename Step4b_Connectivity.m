c17474f20461337eb3bd6535677f18b10d2f6c79
Julie Tseng <julie.a.tseng@gmail.com>
2022-11-17 10:51:47 -0500

    gonna go with the butterworth filter

--- Step4b_Connectivity.m
+++ Step4b_Connectivity.m
@@ -8,24 +8,6 @@ this_output = [config.meta.project_path '/' config.meta.analysis_name '/' pid];
 load([this_output '/out_struct.mat'])
 load([this_output '/step3_data_roi.mat'])
 
-%% MAKE FILTERS
-
-for fq = 1:length(config.step4b.freqbands) % for each frequency band specified in the JSON config file
-    filtband = config.step4b.freqbands(fq,:);
-    transition = mean(filtband) * 0.2;
-    
-    bpfilt{fq} = designfilt('bandpassfir', ...
-                                'StopbandFrequency1', filtband(1) - transition, ...
-                                'PassbandFrequency1', filtband(1), ...
-                                'PassbandFrequency2', filtband(2), ...
-                                'StopbandFrequency2', filtband(2) + transition, ...
-                                'DesignMethod', 'kaiserwin', ...
-                                'PassbandRipple', 0.5, ...
-                                'StopbandAttenuation1', 35, ...
-                                'StopbandAttenuation2', 35, ...
-                                'SampleRate', data_roi.fsample);
-end
-
 %% RUN CONNECTIVITY ANALYSIS
 
 %%% RUN CONNECTIVITY ANALYSIS ---------------------------------------------
@@ -35,22 +17,10 @@ end
     freq.label = data_roi.label;
     
     for fq = 1:length(bpfilt) 
-          data_roi_filt = keepfields(data_roi, {'time', 'fsample', 'label'});
-          for tt = 1:length(data_roi.trial)
-              try
-                this_filtered = arrayfun(@(x) filtfilt(bpfilt{fq}, ...
-                                                        data_roi.trial{tt}(x,:)), ...
-                                                        1:length(data_roi.label), ...
-                                                        'UniformOutput', false);
-              catch
-                  this_filtered = arrayfun(@(x) filter(bpfilt{fq}, ...
-                                                        data_roi.trial{tt}(x,:)), ...
-                                                        1:length(data_roi.label), ...
-                                                        'UniformOutput', false);
-              end
-              data_roi_filt.trial{tt} = cat(1,this_filtered{:});
-          end
-                 
+          cfg = [];
+          cfg.bpfilter = 'yes';
+          cfg.bpfreq = config.step4b.freqbands(fq,:);
+          data_roi_filt = ft_preprocessing(cfg, data_roi);
           
           % select relevant time period
           cfg = [];
